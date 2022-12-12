2c8acfd89957a51b055d793c966fef5201b2b054
carlcherez <carlcherez@gmail.com>
2022-12-11 21:14:01 -0800

    alphabet in capital

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print aphabet in the lowercase, and then in uppercas,
+ * followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
