ec97be972c143cf37f87145b9b5296801cf8a24d
carlcherez <carlcherez@gmail.com>
2022-12-14 20:30:30 -0800

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#findef MAIN.H
+#ifndef MAIN.H
 #define MAIN.H
 int _putchar(char c);
 void print_alphabet(void);
@@ -13,4 +13,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
 #endif
